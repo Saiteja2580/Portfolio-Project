e8ac21dddce317357005b860bc4fefa3c0090580
Saiteja2580 <saitejassp2580@gmail.com>
2024-03-10 12:47:43 +0530

    Responsive nabar created

---
+++ script.js
@@ -0,0 +1,12 @@
+let hamburger = document.querySelector('.fa-solid');
+let list = document.querySelector('ul');
+
+hamburger.addEventListener(
+    'click',function(){
+        if (list.style.display == 'none') {
+            list.style.display = 'block';
+        }else{
+            list.style.display = 'none';
+        }
+    }
+)
