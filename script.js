d6a05b596ab9b0f03772042b78f3342c6985eb5f
Saiteja2580 <saitejassp2580@gmail.com>
2024-03-10 20:13:00 +0530

    made navbar responsive using css

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let hamburger = document.querySelector('.fa-solid');
-let list = document.querySelector('ul');
+// let hamburger = document.querySelector(".fa-solid");
+// let list = document.querySelector("ul");
 
-hamburger.addEventListener(
-    'click',function(){
-        if (list.style.display == 'none') {
-            list.style.display = 'block';
-        }else{
-            list.style.display = 'none';
-        }
-    }
-)
+
+// hamburger.addEventListener("click", function () {
+//   if (list.style.display == "none") {
+//     list.style.display = "block";
+//   } else {
+//     list.style.display = "none";
+//   }
+// });
